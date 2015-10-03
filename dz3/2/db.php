8a79822764e8233b1945879183047e9aa7efecb4
bitrix-dev <evgensr@gmail.com>
2015-10-03 12:57:47 -0700

    dz3 -2

--- dz3/2/db.php
+++ dz3/2/db.php
@@ -2,23 +2,6 @@
 
 include_once 'helper_functions.php';
 
-
-/* Подключение к серверу MySQL */
-$link = mysqli_connect(
-    'localhost',  /* Хост, к которому мы подключаемся */
-    'root',       /* Имя пользователя */
-    '',   /* Используемый пароль */
-    'test1');     /* База данных для запросов по умолчанию */
-
-if (!$link) {
-    printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error());
-    exit;
-}
-
-
-
-
-
 $mysqli = new mysqli("localhost", "root", "", "test1");
 
 /* проверяем соединение */
